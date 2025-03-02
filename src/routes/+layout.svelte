f839bd2f168760bbf8b53b6468520fc3c79ca160
PerchunPak <git@perchun.it>
2025-03-02 15:34:51 +0100

    Add lucide and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import "../app.css";
+  import Header from "./Header.svelte";
+
   let { children } = $props();
 </script>
 
 <div class="grid grid-rows-[auto_1fr_auto]">
-  <header
-    class="bg-surface-950/45 border-b-surface-900 fixed top-0 z-10 flex w-full flex-col space-y-4 border-b-4 p-2 px-4 pr-4 backdrop-blur-md sm:pl-20 lg:pl-40"
-  >
-    (header)
-  </header>
+  <Header />
   <main
     class="h-fit min-h-[calc(100vh-56px)] px-4 pt-20 sm:px-10 md:px-20 lg:px-40"
   >
---
+++ src/routes/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { AppBar } from "@skeletonlabs/skeleton-svelte";
+  import { CreditCard, LogIn, User } from "lucide-svelte";
+</script>
+
+<AppBar
+  classes="fixed top-0 z-10 p-2 sm:pl-20 lg:pl-40 pr-4 border-b-4 border-b-surface-900"
+  padding="px-4"
+  background="backdrop-blur-md bg-surface-950/45"
+  leadSpaceX=""
+  leadClasses="items-center"
+  trailSpaceX=""
+  trailClasses="hidden lg:flex min-h-full items-center gap-2"
+>
+  {#snippet lead()}
+    <a href="/" class="type-title preset-tonal-surface-500 btn !type-scale-6"
+      >Choose A Cat</a
+    >
+  {/snippet}
+  {#snippet trail()}
+    lox
+  {/snippet}
+</AppBar>
