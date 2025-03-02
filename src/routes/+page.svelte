48afc83772500406504477a6278f5bdef11b9c5d
PerchunPak <git@perchun.it>
2025-03-02 20:27:42 +0100

    Add key photos (and keybinds) to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<p class="h-[512px] bg-purple-500 p-4">Paragraph 1</p>
-<p class="h-[512px] bg-purple-500 p-4">Paragraph 2</p>
-<p class="h-[512px] bg-purple-500 p-4">Paragraph 3</p>
+<script lang="ts">
+  import { Image } from "@unpic/svelte";
+  import Keybinds from "./Keybinds.svelte";
+
+  const url = "https://cataas.com/cat";
+  let reloads = $state(0);
+
+  function onevent() {
+    reloads += 1;
+  }
+</script>
+
+<Keybinds onReload={onevent} onLeft={onevent} onRight={onevent} />
+
+{#snippet img(src: string, alt: string)}
+  <div class="flex h-fit w-fit place-content-center rounded border p-4">
+    <Image {src} {alt} layout="fullWidth" />
+  </div>
+{/snippet}
+
+<div class="grid max-h-screen grid-cols-2 space-x-4">
+  {@render img(url + "?".repeat(reloads), "cat1")}
+  {@render img(url + "?".repeat(reloads + 1), "cat1")}
+</div>
---
+++ src/routes/Keybinds.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  type Callback = () => void;
+
+  let {
+    onReload,
+    onRight,
+    onLeft,
+  }: {
+    onReload: Callback;
+    onRight: Callback;
+    onLeft: Callback;
+  } = $props();
+
+  function handleKeyPress(event: KeyboardEvent): void {
+    switch (event.key) {
+      case "r":
+        onReload();
+        event.preventDefault();
+        break;
+
+      case "ArrowRight":
+        onRight();
+        event.preventDefault();
+        break;
+
+      case "ArrowLeft":
+        onLeft();
+        event.preventDefault();
+        break;
+    }
+  }
+
+  function onkeydown(event: KeyboardEvent) {
+    if (event.repeat) return;
+    handleKeyPress(event);
+  }
+</script>
+
+<svelte:window {onkeydown} />
